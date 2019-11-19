c7154a928463f53c959d741d456e463b661739ca
Aman <57857350+asapra-git@users.noreply.github.com>
2019-11-19 21:24:57 +0530

    Add files via upload

---
+++ BaaSDeRegisterVM.ps1
@@ -0,0 +1,21 @@
+﻿param(
+$user,
+$password,
+$VMName,
+$JobName
+)
+
+#  Log file time stamp:
+$LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+#  Log file name:
+$LogFile = 'C:\Scripts\BaaS-PSLogs\'+ $VMName +"LOG_"+$LogTime+".log"
+
+New-Item -Path $LogFile -ItemType "File" -Force
+
+Add-PSSnapin VeeamPSSnapin
+
+Connect-VBRServer -Server VEEAMServer_hostname -User $user -Password $password | Tee-Object -FilePath $LogFile
+
+$output= Get-VBRJob -Name $JobName | Get-VBRJobObject -Name $VMName | Remove-VBRJobObject -Completely
+
+return $output
---
+++ BaaSRegisterVM.ps1
@@ -0,0 +1,27 @@
+﻿param(
+$user,
+$password,
+$VMName,
+$JobName
+)
+
+#  Log file time stamp:
+$LogTime = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+#  Log file name:
+$LogFile = 'C:\Scripts\BaaS-PSLogs\'+ $VMName +"LOG_"+$LogTime+".log"
+
+New-Item -Path $LogFile -ItemType "File" -Force
+
+Add-PSSnapin VeeamPSSnapin
+
+Connect-VBRServer -Server VEEAMServer_hostname -User $user -Password $password | Tee-Object -FilePath $LogFile
+
+$FetchJob = Get-VBRJob -Name $JobName | Tee-Object -FilePath $LogFile
+
+$FetchVM = Find-VBRViEntity -Name $VMName | Tee-Object -FilePath $LogFile
+
+$output= Add-VBRViJobObject -Job $FetchJob -Entities $FetchVM | Tee-Object -FilePath $LogFile
+
+return $output | Tee-Object -FilePath $LogFile
+
+Disconnect-VBRServer
